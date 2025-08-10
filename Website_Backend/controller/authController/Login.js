53ee40aca437096a91dead4a7a21a7603f07f814
Sampann Neema <145037255+Sampann100@users.noreply.github.com>
2025-08-10 17:42:44 +0530

    Update Login.js

--- Website_Backend/controller/authController/Login.js
+++ Website_Backend/controller/authController/Login.js
@@ -43,7 +43,7 @@ exports.loginPage = async (req, res) => {
         .cookie("token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          sameSite: "strict",
+          sameSite: "none",
           maxAge: 24 * 60 * 60 * 1000,
         })
         .status(201)
@@ -93,7 +93,7 @@ exports.loginPage = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       })
       .status(201)
